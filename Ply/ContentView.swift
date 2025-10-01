89adff7cd817306053cf2273f36a60d47c81d377
Rob Gotlieb <rbgotlieb@gmail.com>
2025-10-01 13:01:09 +0100

    Initial Commit

---
+++ Ply/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ply
+//
+//  Created by Rob Gotlieb on 01/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ply/PlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlyApp.swift
+//  Ply
+//
+//  Created by Rob Gotlieb on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
